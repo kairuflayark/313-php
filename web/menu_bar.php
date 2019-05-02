953757b50a90e4ce6e879b715823a23c1102e53d
kairuflayark <cal12011@byui.edu>
2019-05-01 22:02:35 -0600

    menu

--- web/menu_bar.php
+++ web/menu_bar.php
@@ -1,21 +1,25 @@
+<h1> Mountain Root Beer </h1>
+
 <?php
-    if ( $_SERVER['PHP_SELF'] == "/home.php"){
+    $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+    
+    if ( $file == "/home.php"){
         echo "<ul><li><a href=\"home.php\" class=\"current\">Home</a></li>";
     } else {
         echo "<ul><li><a href=\"home.php\">Home</a></li>";
     }
-    if ( $_SERVER['PHP_SELF'] == "\about-us.php") {
+    if ( $file == "\about-us.php") {
         echo "<li><a href=\"about-us.php\" class=\"current\">About Us</a></li>";
     } else {   
         echo "<li><a href=\"about-us.php\">About Us</a></li>";
     }
-    if ( $_SERVER['PHP_SELF'] == "/login.php") {
+    if ( $file == "/login.php") {
         echo "<li><a href=\"login.php\">Login</a></li>";
     } else {
         echo "<li><a href=\"login.php\" class=\"current\">Login</a></li>";
     }
     echo "</ul>";
-    echo $_SERVER['PHP_SELF'];
+    echo $file;
     echo "<br>";
 echo $_SERVER['SERVER_NAME'];
 echo "<br>";
