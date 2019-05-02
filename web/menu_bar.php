b678e713a6a26182de3374c687c4e3a2f9d58529
kairuflayark <cal12011@byui.edu>
2019-05-01 22:10:29 -0600

    menu issues

--- web/menu_bar.php
+++ web/menu_bar.php
@@ -2,21 +2,20 @@
 
 <?php
     $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
-    
-    if ( $file == "/home.php"){
-        echo "<ul><li><a href=\"home.php\" class=\"current\">Home</a></li>";
+    if ( $file === 'home'){
+        echo "<ul><li class=\"current\"><a href=\"home.php\" >Home</a></li>";
     } else {
         echo "<ul><li><a href=\"home.php\">Home</a></li>";
     }
-    if ( $file == "\about-us.php") {
-        echo "<li><a href=\"about-us.php\" class=\"current\">About Us</a></li>";
+    if ( $file === 'about-us') {
+        echo "<li class=\"current\"><a href=\"about-us.php\" >About Us</a></li>";
     } else {   
         echo "<li><a href=\"about-us.php\">About Us</a></li>";
     }
-    if ( $file == "/login.php") {
-        echo "<li><a href=\"login.php\">Login</a></li>";
+    if ( $file === 'login.php') {
+        echo "<li class=\"current\"><a href=\"login.php\">Login</a></li>";
     } else {
-        echo "<li><a href=\"login.php\" class=\"current\">Login</a></li>";
+    <li><a href="login.php" class=>Login</a></li>";
     }
     echo "</ul>";
     echo $file;
