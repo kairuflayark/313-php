9dfd520636be4a3496327527ee52632da5a3b7b8
kairuflayark <cal12011@byui.edu>
2019-05-01 20:02:17 -0600

    Changes styles,

--- web/menu_bar.php
+++ web/menu_bar.php
@@ -1,17 +1,20 @@
 <?php
-    echo "<ul><li><a href=\"home.php\">Home</a></li>";
-    echo "<li><a href=\"about-us.php\">About Us</a></li>";
-    echo "<li><a href=\"login.php\">Login</a></li>";
+    if ( $_SERVER['PHP_SELF'] == "/home.php"){
+        echo "<ul><li><a href=\"home.php\" class=\"current\">Home</a></li>";
+    } else {
+        echo "<ul><li><a href=\"home.php\">Home</a></li>";
+    }
+    if ( $_SERVER['PHP_SELF'] == "\about-us.php") {
+        echo "<li><a href=\"about-us.php\" class=\"current\">About Us</a></li>";
+    } else {   
+        echo "<li><a href=\"about-us.php\">About Us</a></li>";
+    }
+    if ( $_SERVER['PHP_SELF'] == "/login.php") {
+        echo "<li><a href=\"login.php\">Login</a></li>";
+    } else {
+        echo "<li><a href=\"login.php\" class=\"current\">Login</a></li>";
+    }
     echo "</ul>";
-    echo $_SERVER['PHP_SELF'];
-    echo "<br>";
-    echo $_SERVER['SERVER_NAME'];
-    echo "<br>";
-    echo $_SERVER['HTTP_HOST'];
-    echo "<br>";
-    echo $_SERVER['HTTP_REFERER'];
-    echo "<br>";
-    echo $_SERVER['HTTP_USER_AGENT'];
-    echo "<br>";
-    echo $_SERVER['SCRIPT_NAME'];
+     $_SERVER['PHP_SELF'];
+    
     ?>
