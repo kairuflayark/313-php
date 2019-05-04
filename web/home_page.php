7e671ef95b2fd3531d32a1693ae293266fbff9f7
kairuflayark <cal12011@byui.edu>
2019-05-04 16:00:48 -0600

    Added homepage

---
+++ web/home_page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>James Call's Home Page.
+	</title>
+
+	
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="jumbotron text-center">
+		<h1>James Call's Home Page</h1>
+	</div>
+	<?php include("nav_bar.php"); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>About Me</h2>
+				<img src="self_image.jpg" alt="a picture of me at SLC FanX 	2017">
+			</div>
+			<div class="col-sm-8">
+				<h2>TITLE HEADING</h2>
+	      		<h5>Title description, Dec 7, 2017</h5>
+	      		
+	      		<p>Some text..</p>
+	      		<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+	      	</div>
+      	</div>
+  	</div>
+
+
+
+</body>
+</html>
---
+++ web/nav_bar.php
@@ -0,0 +1,13 @@
+<?php
+	$page = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);?>
+
+	<nav class="navbar navbar_expand-sm bg-dark navbar-dark">
+		<a href="home_page.php">Home</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibeNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button> 
+		<div class="collapse navbar-collapse" id="collapsibeNavbar">
+			<ul class="navbar_nav">
+				<li class="nav-item <?php if ($page === 'assignments') {echo 'active';} ?>">
+					<a class="nav-link" href="assignments.php"> assignments</a>
+	</nav>
