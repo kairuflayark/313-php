aef59f886c33504a79e4ab1a4e44f8772fce1dd1
kairuflayark <cal12011@byui.edu>
2019-05-09 16:26:00 -0600

    col

--- web/03prove/browse_items.php
+++ web/03prove/browse_items.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<title>Blender Heaven</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -10,16 +11,17 @@
 </head>
 <body>
 	<div class="container">
-		<div class="col-sm-4">
-			<p>sidebar</p>
+		<div class="row">
+			<div class="col-sm-4">
+				<p>sidebar</p>
+			</div>
+			<div class="col-8">
+				<img src="Binastone.jpg" class="float-left">
+				<h3>Binastone Blender</h3>
+				<p> Budget Blender for the Budget Masses.</p>
+			</div>
 		</div>
-		<div class="col-8">
-			<img src="Binastone.jpg" class="float-left">
-			<h3>Binastone Blender</h3>
-			<p> Budget Blender for the Budget Masses.</p>
-		</div>
-
-</div>
+	</div>
 
 
 </body>
