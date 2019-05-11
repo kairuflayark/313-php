68cc5b754e7f1a391f01d37dd28f2465237b2b29
kairuflayark <cal12011@byui.edu>
2019-05-11 15:57:36 -0600

    session

--- web/03prove/session.php
+++ web/03prove/session.php
@@ -2,6 +2,7 @@
 session_start();
 $blenders = array("Binastone", "Ninja", "Electrolux", "Philips");
 $_SESSION['Blenders'] = $blenders;
+$_SESSION['orange'] = 2;
 foreach ($_SESSION['Blenders'] as $value){
     if( isset($_POST["$value"]) ) {
     // save values from other page to session
