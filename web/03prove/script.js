4094e8e8c1e6215b5af7c2b9a46ebe81e3c58eb8
kairuflayark <cal12011@byui.edu>
2019-05-11 16:01:12 -0600

    script fix

--- web/03prove/script.js
+++ web/03prove/script.js
@@ -1,8 +1,8 @@
-function update_session_value(value) {
+function update_session_value(id, value) {
         $.ajax({
             type: "POST",
             url: 'session.php', // change url as your 
-            data: 'select_amount=' + value,
+            data: id + value,
             dataType: 'json',
             success: function (data) {
 
