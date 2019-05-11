198af1014bb85ae513b683719e94a9e748c806a7
kairuflayark <cal12011@byui.edu>
2019-05-11 17:23:32 -0600

    finished stuff

--- web/03prove/checkout.php
+++ web/03prove/checkout.php
@@ -22,15 +22,17 @@
 			<form>
 				<div class='col-sm-4'>
 					<form action="confirmation.php" method="post">
-					<label>First Name</label>
+					<label>First Name:</label>
 					<input type="text" name="fName">
-					<label>Last Name</label>
+					<label>Last Name:</label>
 					<input type="text" name='lName'>
-					<label>Street Address</label>
+					<label>Street Address:</label>
 					<input type="text" name='street'>
-					<label>State</label>
-					<input type="text" name='state'>
-					<label>Zip Code</label>
+					<label>City:</label>
+					<input type="text" name="city">
+					<label>State:</label>
+					<input type="text" name='state' maxlenght="2">
+					<label>Zip Code:</label>
 					<input type="text" name='zCode'>
 					<button type="submit">Finalize your Order</button>
 					<a href="view_cart.php"><button>Return to Cart</button></a>
--- web/03prove/confirmation.php
+++ web/03prove/confirmation.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +10,50 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<?php
+		function test_input($data) {
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+		}
+	?>
+
+<div class="jumbotron text-center">
+		<h1> Blender Heaven </h1>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<p>Checkout <br> Please Enter your Address</p>
+			</div>
+			<div class="col-sm-4">
+			<h2>Shipped To</h2>
+			<p>
+			<?php
+			foreach ($_SESSION['blenders'] as $value){
+					if ($_SESSION["$value"] == true){
+					echo 	"<div class='item row'>";
+					echo 	"<a  href='view_cart.php?$value=false'><button type='button'><img src='$value.jpg' class='img-fluid float-left'>";
+					echo	"<h3>$value Blender</h3>";
+					echo	"Remove from Cart</button></a>";
+					echo 	"</div>";
+					}
+				}
+					?>
+			<h2> Items to be shipped</h2>
+			<?php
+				foreach ($_SESSION['blenders'] as $value){
+					if ($_SESSION["$value"] == true){
+					echo 	"<div class='item row'>";
+					echo 	"<img src='$value.jpg' class='img-fluid float-left'>";
+					echo	"<h3>$value Blender</h3>";
+					echo 	"</div>";
+					}
+				}
+
+			?>
+
 
 </body>
 </html>
