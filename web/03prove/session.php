1350606e79eb050d81b93d649981765f23f363c6
kairuflayark <cal12011@byui.edu>
2019-05-11 23:12:19 -0600

    script

--- web/03prove/session.php
+++ web/03prove/session.php
@@ -3,9 +3,15 @@ session_start();
 $blenders = array("Binastone", "Ninja", "Electrolux", "Philips");
 $_SESSION['Blenders'] = $blenders;
 $_SESSION['orange'] = 2;
+$_SESSION['address'] = array("fName","lName","street","city","state","zCode");
 foreach ($_SESSION['Blenders'] as $value){
     if( isset($_POST["$value"]) ) {
     // save values from other page to session
+        $_SESSION["$value"] = $_POST["$value"];
+    }
+}
+foreach ($_SESSION['address'] as $value){
+    if (isset($_POST["$value"])){
         $_SESSION["$value"] = $_POST["$value"];
     }
 }
