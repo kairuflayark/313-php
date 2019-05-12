35a07c87a6d25c212a3ab18e176bba0839b2a7c6
kairuflayark <cal12011@byui.edu>
2019-05-11 23:01:20 -0600

    script

--- web/03prove/checkout.php
+++ web/03prove/checkout.php
@@ -9,6 +9,7 @@
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src=script.js></script>
 </head>
 <body>
 <div class="jumbotron text-center">
