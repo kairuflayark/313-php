6651914ed5e116d9b9bdcf2ebcef4b52fce72e36
kairuflayark <cal12011@byui.edu>
2019-05-28 15:40:16 -0600

    sqk

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,21 +1,46 @@
+
+CREATE TABLE politics (
+    id                  serial primary key,
+    affiliation         TEXT
+);  
+
+CREATE TABLE climate (
+    id                  serial primary key,
+    climate_type        TEXT
+);
+
+CREATE TABLE planet_type (
+    id                  serial primary key,
+    planet_type         TEXT
+);
+
+
 CREATE TABLE star_system (
-    star_system_ID      INT PRIMARY key,
-    star_ID             INTEGER REFERENCES star(star_ID),
-    affiliation_ID      text REFERENCES politics(affiliation_ID),
+    id                  serial PRIMARY key,
+    name                text,
+    affiliation         text REFERENCES politics(id)
 );
 
 CREATE TABLE star (
-    star_ID             INT PRIMARY KEY,
-    star_name           TEXT,
+    id                  serial PRIMARY KEY,
+    name                TEXT,
+    star_system         int REFERENCES star_system(id),
     spectral_class      text,
     mass                DECIMAL,
     radius              DECIMAL,
-    luminosity          DECIMAL,
-    planet_id           INTEGER,
+    luminosity          DECIMAL
+);
+
+CREATE TABLE location (
+    id                  serial,
+    star_system         int REFERENCES star_system(id),
+    orbit_star          int REFERENCES star(id)
 );
 
 CREATE TABLE asteroid_belt (
-    belt_id             int PRIMARY key,
+    id                  serial PRIMARY key,
+    name                TEXT,
+    location            int REFERENCES location(id),
     orbit_distance      BIGINT,
     belt_width          int,
     resource_value      int,
@@ -23,69 +48,43 @@ CREATE TABLE asteroid_belt (
 );
 
 CREATE TABLE planet (
-    planet_id           INT PRIMARY KEY,
-    planet_type         TEXT REFERENCES planet_type(planet_type_ID),
+    id                  serial PRIMARY KEY,
+    planet_type         TEXT REFERENCES planet_type(id),
     planet_name         text,
+    location            int REFERENCES location(id),
     orbit_distance      BIGINT,
     planet_radius       INT,
     gravity             DECIMAL,
     orbit_period        DECIMAL,
     rotation            DECIMAL,
-    climate_type        TEXT REFERENCES climate(climate_type),
+    climate_type        TEXT REFERENCES climate(id),
     mean_temp           int,
     water_index         DECIMAL,
     resource_value      int,
     habitability        int,
     population          int,
-    affiliation_ID      text REFERENCES politics(affiliation_ID),
-    moon_ID             int REFERENCES moons(moon_ID)
-);
-
-CREATE TABLE moons (
-    planet_type         TEXT REFERENCES planet_type(planet_type_ID),
-    planet_name         text,
-    orbit_distance      BIGINT,
-    planet_radius       INT,
-    gravity             DECIMAL,
-    orbit_period        DECIMAL,
-    rotation            DECIMAL,
-    climate_type        TEXT REFERENCES climate(climate_type),
-    mean_temp           int,
-    water_index         DECIMAL,
-    resource_value      int,
-    habitability        int,
-    population          int,
-    affiliation_ID      text REFERENCES politics(affiliation_ID),
+    affiliation_ID      text REFERENCES politics(id)
 );
 
 CREATE TABLE fleet(
-    fleet_ID            int primary key,
+    id                  serial primary key,
     fleet_name          text,
     commanding_officer  TEXT,
-    affiliation_ID      text REFERENCES politics(affiliation_ID),
-    ship_ID             int REFERENCES ships(ship_ID),
-    current_location    int REFERENCES planet(planet_id)
+    affiliation_ID      text REFERENCES politics(id),
 );
 
 CREATE TABLE ships(
-    ship_ID             int primary key,
+    id                  serial primary key,
     ship_name           text,
     ship_type           text,
     commanding_officer  text,
+    fleet               text REFERENCES fleet(id),
+    affiliation_ID      text REFERENCES politics(affiliation_ID),
+    current_location    int REFERENCES location(id),
     ship_size           int,
-    weapons             text,
     crew_size           int,
-    affiliation_ID      text REFERENCES politics(affiliation_ID)
+    weapons             text,
+    parasite_craft      text
 );
 
-CREATE TABLE politics (
-    affiliation_ID      TEXT PRIMARY KEY,
-);  
-
-CREATE TABLE climate (
-    climate_type        TEXT primary key,
-);
-
-CREATE TABLE planet_type (
-    planet_type_ID      TEXT PRIMARY KEY,
-);
+ALTER TABLE location ADD orbit_planet int REFERENCES planet(id)
