88c079a4a62d86a7699986b27612ef62ae02e205
kairuflayark <cal12011@byui.edu>
2019-06-06 16:22:35 -0600

    error problems

--- web/star_system/starship_list.php
+++ web/star_system/starship_list.php
@@ -10,25 +10,25 @@
     
   <?php
   try{
-    $dbUrl = getenv('DATABASE_URL');
+      $dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+      $dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $ex)
-{
-  echo 'Error Connecting!';
-  die();
-}
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error Connecting!' . $ex;
+      die();
+    }
 
         foreach ($db->query('SELECT * from fleet') as $fleet){
           echo $fleet['fleet_name'] . $fleet['commanding_officer'];
