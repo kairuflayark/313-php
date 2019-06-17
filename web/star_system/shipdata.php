00d89b1bc92e9a9772246dfab48c941026519ba7
kairuflayark <cal12011@byui.edu>
2019-06-17 13:11:21 -0600

    update

--- web/star_system/shipdata.php
+++ web/star_system/shipdata.php
@@ -57,7 +57,7 @@
             }
 
             echo "</input></td";
-            echo "<td><a href='list.php'><button onClick='update_location($ship, getElementbyID('location').value)' >Update</input></a>";
+            echo "<td><a href='list.php'><button onClick='update_location($ship, getElementbyID('location').value)' >Update</button></a>";
         }
     }
 
