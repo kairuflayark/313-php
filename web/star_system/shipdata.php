517caf76b1a63a0d43daa7f08b15f7d30c8b50f8
kairuflayark <cal12011@byui.edu>
2019-06-18 14:27:43 -0600

    JS

--- web/star_system/shipdata.php
+++ web/star_system/shipdata.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="web\star_system\script.js"></script>
+    <script type="text/javascript" src="script.js"></script>
     <title>Ship Data</title>
 </head>
 <body>
