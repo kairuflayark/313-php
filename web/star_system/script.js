21b2013bd28cf5c8b3abba7b0ddf2b8df2782ec5
kairuflayark <cal12011@byui.edu>
2019-06-19 14:31:16 -0600

    error log

--- web/star_system/script.js
+++ web/star_system/script.js
@@ -5,12 +5,13 @@ function update_location (ship, location)
     console.log(loadU);
     
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function(data) {
         if (this.readyState == 4 && this.status == 200) {
-    
+            console.log(data);
         }
     };
     xmlhttp.open("POST", 'update.php', true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-url-encoded");
     xmlhttp.send(loadU);
+
 }
