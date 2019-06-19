06e7dd7bc7bc6cfd25827d3ce229eb7b50ba3f4c
kairuflayark <cal12011@byui.edu>
2019-06-19 14:33:49 -0600

    error message

--- web/star_system/script.js
+++ web/star_system/script.js
@@ -5,9 +5,9 @@ function update_location (ship, location)
     console.log(loadU);
     
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(data) {
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(data);
+            console.log(this.responseText);
         }
     };
     xmlhttp.open("POST", 'update.php', true);
--- web/star_system/update.php
+++ web/star_system/update.php
@@ -7,7 +7,7 @@
         $ship = $_POST['ship_id'];
 
         $db->query("UPDATE ships SET location_id = $location where ship_id = $ship");
-        echo $ship . $location;
+        print $ship . $location;
     }
 
 
