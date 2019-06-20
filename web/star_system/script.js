eaa2079c6e19ab7ba17fc964d0748cc133865608
kairuflayark <cal12011@byui.edu>
2019-06-20 15:46:36 -0600

    add ship

--- web/star_system/script.js
+++ web/star_system/script.js
@@ -23,4 +23,14 @@ function add_ship(params) {
         loadU += "&" + elem[i].name + "=" + elem[i].value;
     }
     console.log(loadU);
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xmlhttp.open("GET", loadU, true);
+    xmlhttp.send();
+
 }
--- web/star_system/update.php
+++ web/star_system/update.php
@@ -1,4 +1,12 @@
 <?php
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
     include "connect.php";
     print_r($_GET);
     if (isset($_GET['ship_id']))
@@ -10,6 +18,31 @@
         $db->query("UPDATE ships SET location_id = $location where ship_id = $ship");
         print $ship . $location;
     }
-    else 
+    else {
+        $printout = array('ship_name', 'ship_type_id', 'commanding_officer', 'fleet_id', 'affiliation_id', 'location_id', 'ship_size', 'crew_size');
+        $list = array();
+        $end = end($printout);
+        $queryline = 'INSERT INTO ships (';
+        foreach ($printout as $column){
+            if ($end != $column){
+                $queryline . $column . ", ";
+            }
+            else {
+                $queryline . $colum . ") VALUES (";
+            }
+        }
+        foreach ($printout as $column){
+            if ($end != $column){
+                $queryline . test_input($_GET[$column]) . ", ";
+            }
+            else {
+                $queryline . test_input($_GET[$column]) . ")" ;
+            }
+        }
+
+        print $queryline;
+
+
+    }
 
     ?>
