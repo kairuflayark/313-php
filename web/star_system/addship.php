02c72f5e399288c23b01d049161324207eda0751
kairuflayark <cal12011@byui.edu>
2019-06-20 15:16:54 -0600

    Add ship

--- web/star_system/addship.php
+++ web/star_system/addship.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add ship</title>
+    <script type="text/javascript" src="script.js"></script>
+    <title>Add Ship</title>
 </head>
     <form id="data">
     <table>
