e243a6aa06a8ffefec48edb233a2f9313f1df8f7
kairuflayark <cal12011@byui.edu>
2019-06-20 16:37:12 -0600

    update location

--- web/star_system/script.js
+++ web/star_system/script.js
@@ -1,7 +1,7 @@
-function update_location (ship, location)
+function update_location (ship)
 {
   
-    var loadU = 'update.php?ship_id=' + ship + '&location_id=' + location;
+    var loadU = 'update.php?ship_id=' + ship + '&location_id=' + document.getElementById(location).value;
     console.log(loadU);
     
     var xmlhttp = new XMLHttpRequest();
